7be6bd42a3c1412a34c0c9d799884294bdc38f57
zhangziqiang <cnu10028@163.com>
2022-02-14 14:41:01 +0800

    创建二分搜索文件

---
+++ src/main/java/com/leedcode/cn/practice/binarySearch/BinarySearch.java
@@ -0,0 +1,14 @@
+package com.leedcode.cn.practice.binarySearch;
+
+import org.junit.Test;
+
+/**
+ * @author： lenovo
+ * @date： 2022/2/14 14:39
+ * @modifiedBy：
+ * @description： 二分搜索总结
+ * @version: 1.0
+ */
+public class BinarySearch {
+    
+}
