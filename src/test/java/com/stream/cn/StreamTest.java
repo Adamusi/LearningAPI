61756df5df4d903d1925ff3ec2bd80a78d102516
zhangziqiang <cnu10028@163.com>
2022-08-05 17:36:03 +0800

    提交

--- src/test/java/com/stream/cn/StreamTest.java
+++ src/test/java/com/stream/cn/StreamTest.java
@@ -1,11 +1,9 @@
 package com.stream.cn;
 
-import com.cn.demo.User;
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,5 +31,20 @@ public class StreamTest {
         String [] str = {"1","2","3"};
         List<String> dtoList = new ArrayList<String>(Arrays.asList(str).size());
         System.out.println(dtoList);
+    }
+
+    @Test
+    public void test2() {
+        List<Map<String, Object>> bizList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map  = new HashMap<>();
+        Map<String, Object> map2  = new HashMap<>();
+        map.put("parentorgid","123");
+        map.put("Test","123");
+        map2.put("parentorgid","1234");
+        map2.put("Test","123");
+        bizList.add(map);
+        bizList.add(map2);
+        List<String> vidList = bizList.stream().map(item -> String.valueOf(item.get("parentorgid"))).collect(Collectors.toList());
+        System.out.println(vidList);
     }
 }
